340daa8fb033bfd36b86af3b76738d47df9fb336
ST2712 <2220211016@estudiantesunibague.edu.co>
2024-09-27 10:56:35 -0600

    Add: Controller

---
+++ src/main/java/com/bootcamp_2024_2/emazon_shopping_cart/infrastructure/input/rest/ShoppingCarRestController.java
@@ -0,0 +1,26 @@
+package com.bootcamp_2024_2.emazon_shopping_cart.infrastructure.input.rest;
+
+import com.bootcamp_2024_2.emazon_shopping_cart.application.dto.request.ShoppingCarRequest;
+import com.bootcamp_2024_2.emazon_shopping_cart.application.dto.response.ShoppingCarResponse;
+import com.bootcamp_2024_2.emazon_shopping_cart.application.handler.ShoppingCarHandler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/shopping_cart")
+public class ShoppingCarRestController {
+
+    private final ShoppingCarHandler shoppingCarHandler;
+
+    @GetMapping(path = "/v1/shopping_cart/{id}")
+    public ShoppingCarResponse getShoppingCar(@PathVariable Long id) {
+        return shoppingCarHandler.findById(id);
+    }
+
+    @PostMapping(path = "/v1/shopping_cart")
+    public ShoppingCarResponse saveShoppingCart(@RequestBody ShoppingCarRequest request) {
+        return shoppingCarHandler.save(request);
+    }
+
+}
